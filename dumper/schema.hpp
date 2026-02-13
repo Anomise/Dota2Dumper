ae1ee33935993456cff05f69fe1f7db6e944c6a8
Anomise <79972271+Anomise@users.noreply.github.com>
2026-02-13 11:02:19 +0500

    Refactor schema.hpp by removing unused code

--- dumper/schema.hpp
+++ dumper/schema.hpp
@@ -6,33 +6,9 @@
 
 #include <Windows.h>
 #include <cstdint>
-#include <cstring>
-#include <string>
-#include <vector>
 #include <cstdio>
 
-template <typename Ret, typename... Args>
-Ret CallVFunc(void* inst, int idx, Args... args) {
-    using Fn = Ret(__thiscall*)(void*, Args...);
-    auto vt = *reinterpret_cast<void***>(inst);
-    return reinterpret_cast<Fn>(vt[idx])(inst, args...);
-}
-
-
-inline bool IsValidPtr(const void* ptr) {
-    if (!ptr) return false;
-    MEMORY_BASIC_INFORMATION mbi{};
-    if (VirtualQuery(ptr, &mbi, sizeof(mbi)) == 0) return false;
-    if (mbi.State != MEM_COMMIT) return false;
-    if (mbi.Protect & (PAGE_NOACCESS | PAGE_GUARD)) return false;
-    return true;
-}
-
-inline bool IsValidString(const char* str, size_t maxLen = 256) {
-    if (!IsValidPtr(str)) return false;
-    size_t len = strnlen(str, maxLen + 1);
-    return len > 0 && len <= maxLen;
-}
+#include "seh_helper.h"
 
 struct SchemaField_t {
     const char* m_name;       // 0x00
@@ -42,58 +18,28 @@ struct SchemaField_t {
     void*       m_meta;       // 0x18
 };
 
+struct BindingLayout {
+    int name_offset;        // offset of const char* m_name
+    int size_offset;        // offset of int32 m_size
+    int field_count_offset; // offset of int16 m_field_count
+    int fields_offset;      // offset of SchemaField_t* m_fields
+    int parent_offset;      // offset of parent binding ptr
+    bool detected;
+};
 
-class CSchemaClassBinding {
-public:
-    void*           m_parent;       // 0x00
-    const char*     m_binary_name;  // 0x08
-    const char*     m_module_name;  // 0x10
-    int32_t         m_size;         // 0x18
-    int16_t         m_field_count;  // 0x1C
-    int16_t         m_pad1;         // 0x1E
-    int32_t         m_pad2;         // 0x20
-    uint64_t        m_pad3;         // 0x24
-    SchemaField_t*  m_fields;       // 0x28
-
-    const char* GetName() const {
-        if (!IsValidPtr(m_binary_name)) return nullptr;
-        return m_binary_name;
-    }
-
-    int16_t GetFieldCount() const {
-        return m_field_count;
-    }
-
-    SchemaField_t* GetFields() const {
-        if (!IsValidPtr(m_fields)) return nullptr;
-        return m_fields;
-    }
-
-    const char* GetParentName() const {
-        if (!IsValidPtr(m_parent)) return nullptr;
-        auto* p = reinterpret_cast<const CSchemaClassBinding*>(m_parent);
-        if (!IsValidPtr(p->m_binary_name)) return nullptr;
-        return p->m_binary_name;
-    }
+struct SchemaVFuncIndices {
+    int findTypeScopeForModule;  // CSchemaSystem
+    int findDeclaredClass;       // CSchemaSystemTypeScope
+    bool detected;
 };
 
 
-class CSchemaSystemTypeScope {
-public:
-    CSchemaClassBinding* FindDeclaredClass(const char* name) {
-        return CallVFunc<CSchemaClassBinding*>(this, 2, name);
-    }
-};
+extern void*              g_pSchema;
+extern BindingLayout      g_BindingLayout;
+extern SchemaVFuncIndices g_VFuncIdx;
 
-
-class CSchemaSystem {
-public:
-    CSchemaSystemTypeScope* FindTypeScopeForModule(const char* mod) {
-        return CallVFunc<CSchemaSystemTypeScope*>(this, 13, mod, nullptr);
-    }
-};
-
-
-extern CSchemaSystem* g_pSchema;
 bool SchemaInit();
 void* GetInterface(const char* moduleName, const char* ifaceName);
+
+void* Schema_FindTypeScope(const char* moduleName);
+void* Schema_FindClass(void* scope, const char* className);
