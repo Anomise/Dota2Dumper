a727d9c62a900447ca4af358e5a040e51176e1e8
Anomise <79972271+Anomise@users.noreply.github.com>
2026-02-13 11:21:01 +0500

    Update schema.hpp

--- dumper/schema.hpp
+++ dumper/schema.hpp
@@ -11,28 +11,30 @@
 #include "seh_helper.h"
 
 struct SchemaField_t {
-    const char* m_name;       // 0x00
-    void*       m_type;       // 0x08
-    int32_t     m_offset;     // 0x10
-    int32_t     m_meta_count; // 0x14
-    void*       m_meta;       // 0x18
+    const char* m_name;
+    void*       m_type;
+    int32_t     m_offset;
+    int32_t     m_meta_count;
+    void*       m_meta;
 };
 
 struct BindingLayout {
-    int name_offset;        // offset of const char* m_name
-    int size_offset;        // offset of int32 m_size
-    int field_count_offset; // offset of int16 m_field_count
-    int fields_offset;      // offset of SchemaField_t* m_fields
-    int parent_offset;      // offset of parent binding ptr
+    int name_offset;
+    int size_offset;
+    int field_count_offset;
+    int fields_offset;
+    int parent_offset;
     bool detected;
 };
 
 struct SchemaVFuncIndices {
-    int findTypeScopeForModule;  // CSchemaSystem
-    int findDeclaredClass;       // CSchemaSystemTypeScope
+    int findTypeScopeForModule;
+    int findDeclaredClass;
+    int findDeclaredClassNArgs;
+    int classNameOffset;
+    int classInfoPtrOffset;
     bool detected;
 };
-
 
 extern void*              g_pSchema;
 extern BindingLayout      g_BindingLayout;
@@ -40,6 +42,5 @@ extern SchemaVFuncIndices g_VFuncIdx;
 
 bool SchemaInit();
 void* GetInterface(const char* moduleName, const char* ifaceName);
-
 void* Schema_FindTypeScope(const char* moduleName);
 void* Schema_FindClass(void* scope, const char* className);
