16dc0dcb54b4ce1eac234334b16921885bf8d5a8
Anomise <79972271+Anomise@users.noreply.github.com>
2026-02-13 11:34:53 +0500

    Add SEH_IsAsciiString function to seh_helper.h

--- dumper/seh_helper.h
+++ dumper/seh_helper.h
@@ -7,26 +7,15 @@ extern "C" {
 #include <Windows.h>
 #include <stdint.h>
 
-
 void* SEH_VCall1(void* instance, int index, const char* arg1);
-
-
 void* SEH_VCall2(void* instance, int index, const char* arg1, void* arg2);
-
 int SEH_ReadPtr(const void* base, int offset, void** out);
-
-
 int SEH_ReadI32(const void* base, int offset, int32_t* out);
-
-
 int SEH_ReadI16(const void* base, int offset, int16_t* out);
-
 const char* SEH_ReadStr(const void* base, int offset);
-
 int SEH_IsReadable(const void* ptr, size_t size);
-
-
 int SEH_ValidateString(const char* str, size_t maxLen);
+int SEH_IsAsciiString(const char* str, size_t maxLen);
 
 #ifdef __cplusplus
 }
